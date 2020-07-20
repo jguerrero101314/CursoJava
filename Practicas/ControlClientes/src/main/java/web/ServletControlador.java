9c4cd066a34431852fd6a8bd37ba659dc36640c9
Joel Guerrero <joelg1014@hotmail.com>
2020-07-20 10:39:42 -0700

    236. Listado de Clientes - Básico

---
+++ Practicas/ControlClientes/src/main/java/web/ServletControlador.java
@@ -0,0 +1,23 @@
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+    
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+       List<Cliente> clientes = new ClienteDaoJDBC().listar();
+       System.out.println("clientes = " + clientes);
+       req.setAttribute("clientes", clientes);
+       req.getRequestDispatcher("clientes.jsp").forward(req, resp);
+       
+   }
+    
+}
